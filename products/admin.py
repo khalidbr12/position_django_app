ac1c7b352d07652214a5f286588d45f70952da86
khalid <you@example.com>
2024-06-26 14:33:16 +0100

    commit 2

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.site_header = 'Admin Pannel'
+admin.site.site_header = 'Admin Panel'
 admin.site.site_title = 'Docker Socomadis Admin'
 admin.site.index_title = 'Administration'
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'reference', 'position', 'active', 'category')
+    search_fields = ('name', 'reference') 
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
